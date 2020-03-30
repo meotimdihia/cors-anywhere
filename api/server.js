5e9da25905f378026bd5796f61b1892b090a3c10
Dien <meotimdihia@gmail.com>
2020-03-30 21:57:20 +0700

    fixed it for zeit now deploying

--- api/server.js
+++ api/server.js
@@ -1,14 +1,11 @@
-// Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
-
 // Grab the blacklist from the command-line so that we can update the blacklist without deploying
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
-var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+
+const originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
+
+const originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
 function parseEnvList(env) {
   if (!env) {
     return [];
@@ -17,13 +14,13 @@ function parseEnvList(env) {
 }
 
 // Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
-var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
 
-var cors_proxy = require('./lib/cors-anywhere');
-cors_proxy.createServer({
+const checkRateLimit = require('../lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
+
+const cors_proxy = require('../lib/cors-anywhere').createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
+
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
@@ -39,6 +36,14 @@ cors_proxy.createServer({
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
-}).listen(port, host, function() {
-  console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
+})
+
+module.exports = (req, res) => {
+  let url = '/';
+  if (req.query._url) url += req.query._url;
+  else url += 'iscorsneeded';
+  const reqModified = Object.assign(req, {url})
+  
+  cors_proxy.emit('request', reqModified, res);
+};
+
